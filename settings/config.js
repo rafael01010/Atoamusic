b25eaa03ee3aa5d9e784e5687b9f1d754913f11f
rafael01010 <67513151+rafael01010@users.noreply.github.com>
2022-06-03 07:07:59 +0100

    update

--- settings/config.js
+++ settings/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   TOKEN:
     process.env.TOKEN ||
-    "BotToken",
+    "OTExNjYwMzcwNjE4MzU5ODA4.GG7VW3.u1oENc_ytDlRCCv5n_NXpVVOtTgNNyYFipzXKg",
   PREFIX: process.env.PREFIX || ".",
   embed: {
     color: process.env.color || "#220f80",
